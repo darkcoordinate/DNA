d614ce0c8f34b6dd2f4deccb2eebe20a6ba24894
darkcoordinate <sagacious112358@gmail.com>
2017-03-06 14:43:35 +0530

    Mon Mar  6 14:43:35 IST 2017

--- a.sh
+++ a.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "hello"
-if g++ test4.cpp -I ./ -o data ;then
-	./data
+COMPILER=g++
+OUT_FILE=com
+COM_FILE=test.cpp
+OBJ_FILE=pt1.obj
+if $COMPILER -g $COM_FILE -I ./ -o $OUT_FILE ;then
+	./$OUT_FILE $OBJ_FILE
 fi
--- test.cpp
+++ test.cpp
@@ -141,11 +141,13 @@ void quick_sort(vector<pointer*> &A, vector<int> &B, int start, int end, int p_1
 
 
 int partition_line(vector<line*> &A, vector<int> &B, int start, int end){
+
 	int i = start +1;
 	line* piv = A[B[start]];
 	for(int j =  start + 1; j <= end; j++){
 		if(A[B[j]]->min_x() < piv->min_x()){
 			swap(B[i], B[j]);
+			cout<<j<<endl;
 			i+= 1;
 		}
 	}
@@ -156,24 +158,19 @@ int partition_line(vector<line*> &A, vector<int> &B, int start, int end){
 void quick_sort_line(vector<line*> &A, vector<int> &B, int start, int end){
 	if(start < end){
 		int piv_pos = partition_line(A,B,start, end);
-		quick_sort_line(A,B,start,end);
 		quick_sort_line(A,B,start,piv_pos - 1);
+		quick_sort_line(A,B,piv_pos + 1, end);
 	}
 }
-int main(int argc, char** argv){
-	ifstream file(argv[1]);
+void read(char* argv, vector<pointer*> &Points, vector<line*> &lineset){
+	ifstream file(argv);
 	string line2;
-	vector<pointer*> Points(1000);
-	vector<line*> lineset(1000);
 	int pointCounter = 0, linesetCounter = 0;
-	double max_x =0,max_y =0, max_z = 0, min_x = 0, min_y = 0, min_z = 0;
 	for(int i = 0; i < 1000; i++){
 		if(getline(file,line2) == NULL){
 			break;
 		}
 		const char* s = line2.c_str();
-		cout<<"file"<<line2<<endl;
-		cout<<"prog"<<s<<endl;
 		double k, m, n;
 		if(s == NULL){
 			break;
@@ -194,80 +191,18 @@ int main(int argc, char** argv){
 		}
 		//printf("%d %s\n",i, s);
 	}
-	cout<<"Line set counter :"<<linesetCounter<<endl;
 	if(Points.size()> pointCounter){
 		Points.erase(Points.begin() + pointCounter, Points.end());
 	}
 	if( lineset.size() > linesetCounter){
 		lineset.erase(lineset.begin() + linesetCounter, lineset.end());
 	}
-	printf("uihifhihfrer\n");
-	vector<int> asn(pointCounter);
-	vector<int> linar(linesetCounter);
-	//I don't know what I did, so if any one can help please welcome
-	for(int i = 0; i < pointCounter; i++){
-		asn[i] = i;
-	}
-	for(int i = 0; i < linesetCounter; i++){
-		linar[i] = i;
-	}
-	quick_sort(Points,asn,0,pointCounter - 1, 0);
-	for(int i = 0; i < pointCounter; i++){
-		cout<<Points[asn[i]]->k[0]<<" "<<Points[asn[i]]->k[1]<<endl;
-	}
-	for(int i = 0; i < linesetCounter; i++){
-		cout<<lineset[i]->l[0].k[0]<<" "<<lineset[i]->l[0].k[1]<<" "<<lineset[i]->l[1].k[0]<<" "<<lineset[i]->l[1].k[1]<<endl;
-	}
-	cout<< "fair work";
-	quick_sort_line(lineset, linar,0, linesetCounter -1);
-	for(int i = 0; i < linesetCounter; i++){
-		cout<<lineset[linar[i]]->l[0].k[0]<<" "<<lineset[linar[i]]->l[0].k[1]<<" "<<lineset[linar[i]]->l[1].k[0]<<" "<<lineset[linar[i]]->l[1].k[1]<<endl;
-	}
-#ifdef MSG1
-	for(int i = 0; i < 10; i++){
-		cout<<asn[i]<<" "<<point[asn[i]]->k[0]<<" "<<point[asn[i]]->k[1]<<endl;
-	}
-#endif
-#ifdef MSG2
-	cout<<lin.l[0].k[0]<<" "<<lin.l[0].k[1]<<" "<<lin.l[1].k[0]<<" "<<lin.l[1].k[1]<<endl;
-	cout<<lin2.l[0].k[0]<<" "<<lin2.l[0].k[1]<<" "<<lin2.l[1].k[0]<<" "<<lin2.l[1].k[1]<<endl;
-	cout<<lin3.l[0].k[0]<<" "<<lin3.l[0].k[1]<<" "<<lin3.l[1].k[0]<<" "<<lin3.l[1].k[1]<<endl;
-	cout<<lin4.l[0].k[0]<<" "<<lin4.l[0].k[1]<<" "<<lin4.l[1].k[0]<<" "<<lin4.l[1].k[1]<<endl<<endl;
-	for(int i = 0; i < 5; i++){
-		cout<<i<<li[i].l[0].k[0]<<" "<<li[i].l[0].k[1]<<" "<<li[i].l[1].k[0]<<" "<<li[i].l[1].k[1]<<endl;
-	}
-#endif
-	vector<pointer*> point2(linesetCounter);
-	vector<int> not_nul_indx(linesetCounter);
-	int ntnlsz = 0;
-	for(double i = 0; i < pointCounter; i++){
-		pointer p;
-		int mi = 0;
-		int ntnlsz = 0;
-		for(int j = 0; j < linesetCounter; j++){
-			try{
-				//point2[j] = new pointer();
-				//cout<<j<<" "<<i<<" "<<i*0.025<<endl;
-				point2[j] = line_intersection_with(*(lineset[j]),Points[asn[i]]->k[0]);
-				not_nul_indx[ntnlsz] = j;
-				ntnlsz++;
-				//cout<<" "<<point2[j]->k[0]<<" "<<point2[j]->k[1]<<endl;
-				mi = 1;
-			}
-			catch(exception e){
-				//cout<<j<<endl;
-				point2[j] = NULL;
-				mi = 0;
-			}
-		}
-		quick_sort(point2, not_nul_indx,0,ntnlsz -1,1);
-		cout<<endl<<ntnlsz<<endl;
-		for(int j = 0; j < ntnlsz; j++){
-			//cout<<endl;
-			cout<<not_nul_indx[j]<<" "<<point2[not_nul_indx[j]]->k[0]<<" "<<point2[not_nul_indx[j]]->k[1]<<endl;
-		}
-		if(ntnlsz > 0)
-			cout<<endl<<endl;
-	}
+}
+
+
+int main(int argc, char**argv){
+	vector<pointer*> Points(1000);
+	vector<line*> lineset(1000);
+	read(argv[1],Points, lineset);
 	return 0;
 }
